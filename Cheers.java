8ef3952f9545d62b7899f4c8c0098efafb330549
SaarHurvitz <saar.hurvitz@post.runi.ac.il>
2025-11-07 18:35:22 +0200

    Completed Assignment

--- Cheers.java
+++ Cheers.java
@@ -13,7 +13,18 @@ public class Cheers {
                 String s = args[0];
                 //prints the String given by the user letter by letter
 	        for(int i = 0; i < s.length(); i++){
-                System.out.println("Give me " + s.charAt(i) + ": " + s.charAt(i) + "!");
+            System.out.print("Give me ");
+
+            /*If the string contains one of the following letters
+            it will print "an". if not "a". */
+            if("AEFHILMNORSX".indexOf(s.charAt(i)) != -1){
+               System.out.print("an ");
+            }
+            else{
+                System.out.print("a  ");
+            }
+            
+                System.out.println(s.charAt(i) + ": " + s.charAt(i) + "!");
             }
             
             System.out.println("What does it spell?");
