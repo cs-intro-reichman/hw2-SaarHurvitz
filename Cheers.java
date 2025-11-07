330e16dfba267a56af7a186b961c9412d12f2178
SaarHurvitz <saar.hurvitz@post.runi.ac.il>
2025-11-07 17:45:31 +0200

    Completed Assignment

--- Cheers.java
+++ Cheers.java
@@ -7,7 +7,22 @@ Great work boaz!
 
 // Prints a crowd cheering output.
 public class Cheers {
+
         public static void main(String[] args) {
-	    // Replace this comment with your code
+
+                String s = args[0];
+                //prints the String given by the user letter by letter
+	        for(int i = 0; i < s.length(); i++){
+                System.out.println("Give me " + s.charAt(i) + ": " + s.charAt(i) + "!");
+            }
+            
+            System.out.println("What does it spell?");
+
+            int num = Integer.parseInt(args[1]);
+            //prints the String given by the user for the amount of times given by the user
+            for(; num > 0; num--){
+                System.out.println(s + "!");
+            }
+
         }
 }
--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,65 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+
+		int hailstone = Integer.parseInt(args[0]);
+		String verOrCon = args[1];
+		int sequences = hailstone;
+		int num = 0;
+		//counts the amount of actions until it reached 1
+        int actions = 1;
+
+		//prints the option of 1 that will alays happen
+		System.out.println("1 4 2 1 (4)");
+		//creating a hailstone suqeunce out of any numeric number
+		for(int i = 2; i <= hailstone; i++){
+
+		num = i;
+
+	//with sequence output
+	if(verOrCon.equals("v")){
+		while (num != 1) {
+			if(num % 2 == 0){
+				System.out.print(num + " ");
+               num = num / 2;
+			   
+			}
+			else{
+				System.out.print(num + " ");
+				num = num * 3 + 1;
+			}
+
+			actions++;
+
+		}
+
+		//prints 1 at the end
+        System.out.print(1 + " ");
+        //prints the amount of actions
+		System.out.println("(" + (actions) + ")");
+
+	}
+    //without sequence output
+	else if(verOrCon.equals("c")){
+		while (num != 1) {
+			if(num % 2 == 0){
+               num = num / 2;
+			}
+			else{
+				num = num * 3 + 1;
+			}
+
+		}
+
+	}
+
+	//reset the amount of actions for the next loop
+	 actions = 1;
+
+	}
+
+		 System.out.println("Every one of the first " + sequences 
+		                  + " hailstone sequences reached 1.");
+
 	}
 }
